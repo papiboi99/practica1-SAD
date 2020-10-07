5c186f514f12ff8f82c1879730ffcafac130c754
papiboi99 <71660228+papiboi99@users.noreply.github.com>
2020-10-07 17:19:11 +0200

    Add files via upload

--- EditableBufferedReader.java
+++ EditableBufferedReader.java
@@ -1,41 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sad.practica1;
 
-import java.io.BufferedReader;
-import java.io.Reader;
+import java.io.*;
 
-/**
- *
- * @author david.chicano
- */
 public class EditableBufferedReader extends BufferedReader {
 
+    Line line;
+    InputStreamReader in;
+    
     public EditableBufferedReader(Reader reader) {
         super(reader);
     }
     
     public void setRaw(){
-    
+        try{
+        String[] cmd = {"/bin/sh", "-c", "stty raw </dev/tty"};
+        Runtime.getRuntime().exec(cmd).waitFor();
+        }catch(Exception e){
+
+        }
     }
     
     public void unsetRaw(){
-    
+        try{
+        String[] cmd = {"/bin/sh", "-c", "stty cooked </dev/tty"};
+        Runtime.getRuntime().exec(cmd).waitFor();
+        }catch(Exception e){
+
+        }
     }
     
     @Override
-    public int read(){
-        return 0;
+    public int read() throws IOException{
+        line = new Line();
+        char ch;
+        this.setRaw();
         
+        
+        while (true) {
+            ch = (char) super.read();
+            line.setStr(Character.toString(ch));
+            
+            if (ch == '\n') {return 0;}
+        }
     }
     
     @Override
-    public String readLine(){
-        return null;
-        
+    public String readLine()throws IOException{
+        int i = read();
+        return line.getLine();
     }
     
     
--- Line.java
+++ Line.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sad.practica1;
 
-/**
- *
- * @author david.chicano
- */
 public class Line {
+
+//right, left: caràcter dreta, caràcter esquerra amb les fletxes.
+//home, end: principi, final de línia.
+//ins: commuta mode inserció/sobre-escriptura.
+//del, bksp: esborra caràcter actual o caràcter a l’esquerra.
+    
+    static int home = 0;
+    int cursor;
+    int end;
+    String str;
+    boolean ins;
+    
+    public Line (){
+        cursor = 0;
+        end = 0;
+        str = null;
+        ins = true;
+    }
+    
+    public void setStr (String ch){
+        str = str + ch;
+    }
+    
+    public String getLine (){
+        return str;
+    }
+    
+    public void right (){
+        
+    }
+    
+    public void left (){
+        
+    }    
+    
+    public void del (){
+        
+    }
+    
+    public void bksp (){
+        
+    }
     
 }
